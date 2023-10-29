a6e2ac4504a632c4d99051574d1dca464ecb88b3
TuanZyy <14451569-TuanZyy@users.noreply.replit.com>
2023-10-29 12:25:11 +0000

    Initial commit

---
+++ broadcast.js
@@ -0,0 +1,41 @@
+'use strict';
+
+switch (global.Fca.Require.FastConfig.BroadCast) {
+    case true: {
+        try {
+            var logger = global.Fca.Require.logger;
+                var Fetch = global.Fca.Require.Fetch;
+                    Fetch.get("https://github.com/TuanZyy/Global_Fca/raw/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
+  //make https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/Fca_BroadCast.json
+                        global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
+                    var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Chúc Các Bạn Sử Dụng FCA Vui Vẻ!";
+                logger.Normal(random);
+            }); 
+        }	
+        catch (e) {
+            console.log(e);
+        }
+        return setInterval(() => { 
+            try {
+                try {
+                    var logger = global.Fca.Require.logger;
+                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Chúc Các Bạn Sử Dụng FCA Vui Vẻ!";
+                    logger.Normal(random);
+                }	
+                catch (e) {
+                    console.log(e);
+                    return;
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        },3600 * 1000);
+    }
+    case false: {
+        break;
+    }
+    default: {
+        break;
+    }
+}
